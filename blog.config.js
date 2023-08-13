b325c9ac08de4d3f99aedddca63b6ec7b5c963ce
Ric Li <ricardomlee@gmail.com>
2023-08-13 10:15:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'Ric\'s Blog',
   author: 'Ric Li',
   email: 'ricardomlee@gmail.com',
-  link: 'https://ricli.link',
+  link: 'https://ricli.tech',
   description: 'TTG awesome site.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
